9940916f4621be62779a6769e191d5a79b7a085a
Daniel Moisa <danimoisa93@gmail.com>
2024-07-08 20:01:01 +0000

    fix: clean

--- web/frontend-spa/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import SearchBar from "@/components/search-bar";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <SearchBar />
-    </main>
-  );
-}
